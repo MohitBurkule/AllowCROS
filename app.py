b914e2b8e911055ad99dde0267cfda5e909da452
mohit <bitbucketfecommon@mylocus.in>
2020-10-09 17:59:42 +0530

    bot error

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ def index(url):
 	'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
 	'Accept-Encoding': 'none',
 	'Accept-Language': 'en-US,en;q=0.8',
-	'Connection': 'keep-alive'}
+	'Connection': 'keep-alive',
+	'X-JAVASCRIPT-ENABLED': 'false'}
 	req = urllib.request.Request(url, headers=hdr)
 	webUrl = urllib.request.urlopen(req, timeout=10)
 	data = webUrl.read()
