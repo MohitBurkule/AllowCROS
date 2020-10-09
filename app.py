a967638f728ab4b19660f677fa94616dbbdd4258
mohit <bitbucketfecommon@mylocus.in>
2020-10-09 17:48:50 +0530

    bot error

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import urllib
 
 @app.route('/<path:url>')
 def index(url):
-	req = urllib.request.Request(url[1:], headers=request.headers)
+	req = urllib.request.Request(url, headers=request.headers)
 	webUrl = urllib.request.urlopen(req, timeout=10)
 	data = webUrl.read()
 	return data
